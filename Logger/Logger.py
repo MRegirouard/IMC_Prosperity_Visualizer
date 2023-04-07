3b88f25be183b50258368029bbfb198d07029975
Ryan Rivera <ryancriv@gmail.com>
2023-04-07 17:36:29 -0400

    Finished commented code

--- Logger/Logger.py
+++ Logger/Logger.py
@@ -27,15 +27,10 @@ class Logger:
             "state": self.compress_state(state),
             "orders": self.compress_orders(orders),
             "logs": self.logs,
-            "values": values
+            "values": values,
         }, cls=ProsperityEncoder, separators=(",", ":"), sort_keys=True))
 
         self.logs = ""
-
-    def compress_values(self, state: TradingState, values: dict[str, Any]) ->dict[str, ]:
-        '''
-        We want 
-        '''
 
     def compress_state(self, state: TradingState) -> dict[str, Any]:
         '''
