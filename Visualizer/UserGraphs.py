309066ece1a6785cb0b756e442bb997dd5724c01
Ethan Girouard <mr.egirouard@gmail.com>
2023-04-26 17:40:53 -0400

    Add user graphs file

---
+++ Visualizer/UserGraphs.py
@@ -0,0 +1,10 @@
+from GraphFuncTypes import *
+from PredefinedGraphs import *
+from LogParser import LogData
+import plotly.graph_objects as go
+import plotly.express as px
+
+# Add your custom graphs here
+
+# Add custom or predefined graph functions to this list to have them show up in the visualizer
+user_graphs: graph_func_list_t = []
