c2fc2638ecee68cb4819f5dbf18a3aea599ebb5b
Ethan Girouard <mr.egirouard@gmail.com>
2023-04-26 22:20:08 -0400

    Make graph generation multithreaded

--- Visualizer/Visualizer.py
+++ Visualizer/Visualizer.py
@@ -3,6 +3,10 @@ from UserGraphs import user_graphs
 from LogParser import parse
 import sys
 import os
+from GraphFuncTypes import graph_func_t
+from threading import Thread
+from typing import List, Tuple
+import plotly.graph_objects as go
 
 if __name__ == "__main__":
     st.set_page_config(layout="wide")
@@ -32,19 +36,40 @@ if __name__ == "__main__":
         st.error("Ensure that the correct logger was used to generate the log file.")
         sys.exit(1)
     
+    # Generate the graphs, asynchronously
+    graph_tasks: List[Thread] = []
+    graphs: List[Tuple[int, go.Figure | str]] = []
+
+    def gen_graph(graph_func: graph_func_t, index: int) -> None:
+        """ 
+        A simple function that tries to generate a graph, and adds it to the list of graphs,
+        or adds an error message if it fails
+        """   
+        try:
+            graphs.append((index, graph_func(data)))
+        except Exception as e:
+            graphs.append((index, "Error generating graph: " + str(e)))
+    
+    # Add a task for each graph function and start the thread
+    for i, graph_func in enumerate(user_graphs):
+        graph_tasks.append(Thread(target=gen_graph, args=(graph_func,i)))
+        graph_tasks[-1].start()
+        
+    # Wait for all the graphs to finish generating
+    for task in graph_tasks:
+        task.join()
+        
+    # Sort the graphs by index, so they show up in the desired order
+    graphs.sort(key=lambda x: x[0])
+    
     # Split the page into two columns
     col1, col2 = st.columns(2)
 
     # Run each graph function, and display it in alternating columns
-    for i, graph_fn in enumerate(user_graphs):
-        try:
-            graph = graph_fn(data)
-            if i % 2 == 0:
-                col1.plotly_chart(graph, use_container_width=True)
-            else:
-                col2.plotly_chart(graph, use_container_width=True)
-        except Exception as e:
-            if i % 2 == 0:
-                col1.warning("Graph " + str(i+1) + " failed to load because of error: " + str(e))
-            else:
-                col2.warning("Graph " + str(i+1) + " failed to load because of error: " + str(e))
+    for i, graph in enumerate(graphs):
+        column = col1 if i % 2 == 0 else col2
+        
+        if isinstance(graph[1], str):
+            column.error(graph[1])
+        else:
+            column.plotly_chart(graph[1], use_container_width=True)
